62aa7e25f6ab5cf18b7d45049c0c7d2c199c56fa
ShivamThukral <shivam13095@iiitd.ac.in>
2016-03-30 07:30:12 +0530

    Sigin Done

---
+++ app/src/main/java/com/example/student/courseexchange/LoginActivity.java
@@ -0,0 +1,185 @@
+package com.example.student.courseexchange;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import butterknife.ButterKnife;
+import butterknife.Bind;
+
+/**
+ * Created by shivam thukral on 3/29/2016.
+ */
+public class LoginActivity extends AppCompatActivity {
+    private static final String TAG = "LoginActivity";
+    private static final int REQUEST_SIGNUP = 0;
+
+    @Bind(R.id.input_email) EditText _emailText;
+    @Bind(R.id.input_password) EditText _passwordText;
+    @Bind(R.id.btn_login) Button _loginButton;
+    //@Bind(R.id.link_signup) TextView _signupLink;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        _loginButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
+       /* _signupLink.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // Start the Signup activity
+             //   Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
+               // startActivityForResult(intent, REQUEST_SIGNUP);
+            }
+        });*/
+    }
+
+    public void login() {
+        Log.d(TAG, "Login");
+
+        if (!validate()) {
+            onLoginFailed();
+            return;
+        }
+
+        _loginButton.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this,
+                R.style.AppTheme_Dark_Dialog);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Authenticating...");
+        progressDialog.show();
+
+        String email = _emailText.getText().toString();
+        String password = _passwordText.getText().toString();
+
+        // TODO: Implement your own authentication logic here.
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        // On complete call either onLoginSuccess or onLoginFailed
+                        onLoginSuccess();
+                        // onLoginFailed();
+                        progressDialog.dismiss();
+                    }
+                }, 3000);
+    }
+boolean containsFiveDigits(String s)
+{
+    boolean containsDigit = false;
+    int count=0;
+    if (s != null && !s.isEmpty()) {
+        for (char c : s.toCharArray()) {
+            if (Character.isDigit(c)) {
+                count++;
+            }
+        }
+    }
+if(count==5)
+    containsDigit=true;
+else
+    containsDigit=false;
+    return containsDigit;
+}
+    boolean checkIfValidIIITDAccount()
+    {
+        boolean valid = false;
+        String iiitd="@iiitd.ac.in";
+        String email = _emailText.getText().toString();
+        String password = _passwordText.getText().toString();
+
+        if(email.toLowerCase().contains(iiitd.toLowerCase()))
+        {
+            if(containsFiveDigits(email))
+            {
+                valid=true;
+            }
+        }
+
+        return valid;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_SIGNUP) {
+            if (resultCode == RESULT_OK) {
+
+                // TODO: Implement successful signup logic here
+                // By default we just finish the Activity and log them in automatically
+                //redirect to new main activity
+
+
+
+                this.finish();
+            }
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Disable going back to the MainActivity
+        moveTaskToBack(true);
+    }
+
+    public void onLoginSuccess() {
+        _loginButton.setEnabled(true);
+        if(checkIfValidIIITDAccount())
+        {
+            Intent myIntent = new Intent(LoginActivity.this, MainActivity.class);
+            String email = _emailText.getText().toString();
+            myIntent.putExtra("email",email ); //Optional parameters
+            LoginActivity.this.startActivity(myIntent);
+        }
+        else
+        {
+            Toast.makeText(getBaseContext(), "Non IIITD User", Toast.LENGTH_LONG).show();
+        }
+       // finish();
+    }
+
+    public void onLoginFailed() {
+        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+
+        _loginButton.setEnabled(true);
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String email = _emailText.getText().toString();
+        String password = _passwordText.getText().toString();
+
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            _emailText.setError("enter a valid email address");
+            valid = false;
+        } else {
+            _emailText.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
+            _passwordText.setError("between 4 and 10 alphanumeric characters");
+            valid = false;
+        } else {
+            _passwordText.setError(null);
+        }
+
+        return valid;
+    }
+}
